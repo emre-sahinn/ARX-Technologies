9dea77e062f8bae284cfe35f5de8ba70bd93974a
ARXINO <survandergames@gmail.com>
2019-01-28 12:16:32 +0300

    Update settings.py

--- Arxtech/settings.py
+++ Arxtech/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@!@#xmzqrf!@ey3p-!y53xdo^ta@o1wtd1p1c@kdx)w9l7d!z('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
